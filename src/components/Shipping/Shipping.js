0dbcaad902ac8ace801571fc794ea55e1cdc13f2
Leonard Lacson <leonardl@dev6.com>
2019-01-11 13:29:23 -0500

    starting point...

--- src/components/Shipping/Shipping.js
+++ src/components/Shipping/Shipping.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { withRouter } from 'react-router-dom';
 import './Shipping.css';
@@ -10,8 +10,10 @@ import { FormWithConstraints, FieldFeedbacks, FieldFeedback, } from 'react-form-
 
 const contactIntro = 'Welcome to the club, where can we ship your shirts to? You can always provide this information at checkout';
 
-function Shipping(props) {
-    const [state, setState] = useState({
+class Shipping extends Component {
+    constructor() {
+        super();
+        this.state = {
             name: '',
             address1: '',
             address2: '',
@@ -22,45 +24,40 @@ function Shipping(props) {
             zip: '',
             email: '',
             password: ''
-    });
-    const signupFormRef = useRef();
-
-    useEffect(() => {
-        setState({
-            email: props.signUpdata.email,
-            password: props.signUpdata.password
-        })
-    }, [props])
-    
-
-    // componentWillReceiveProps(nextProps) {
-    //     this.setState({
-    //         email: nextProps.signUpdata.email,
-    //         password: nextProps.signUpdata.password
-    //     });
-    // }
-
-    const shippingInfoSubmit = (event) => {
-        event.preventDefault()
-        signupFormRef.current.validateFields(event.currentTarget.name);
-        if(signupFormRef.current.isValid()) {
-            props.createUser(state);
-            props.history.push('/catalog');
-        }  
+        };
     }
 
-    const handleInputChange = event => {
-        signupFormRef.current.validateFields(event.currentTarget.name);
-        setState({
-            ...state,
-            [event.target.name]: event.target.value
+    
+
+    componentWillReceiveProps(nextProps) {
+        this.setState({
+            email: nextProps.signUpdata.email,
+            password: nextProps.signUpdata.password
+        });
+    }
+    shippingInfoSubmit = (event) => {
+        event.preventDefault()
+        this.signupForm.validateFields(event.currentTarget.name);
+        if(this.signupForm.isValid()) {
+            this.props.createUser(this.state);
+            this.props.history.push('/catalog');
+        }  
+    }//this.signupForm.validateFields(event.currentTarget.name);
+    handleInputChange = event => {
+        this.signupForm.validateFields(event.currentTarget.name);
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+        this.setState({
+        [name]: value
         });
     }
 
+    render() {
         let postalZipProps = {};
-        let regionsForSelectedCountry = regions[state.country];
+        let regionsForSelectedCountry = regions[this.state.country];
         let provStateLabel = 'Region';
-            switch (state.country) 
+            switch (this.state.country) 
             { 
                 case 'canada':
                     provStateLabel = 'Province';
@@ -95,14 +92,14 @@ function Shipping(props) {
                 <h2>Awesome!</h2>
                 <p>{contactIntro}</p>
                 <FormWithConstraints  
-                    onSubmit={shippingInfoSubmit} 
+                    onSubmit={this.shippingInfoSubmit.bind(this)} 
                     noValidate
-                    ref={signupFormRef}
+                    ref={element => (this.signupForm = element)}
                 >
                     <Row className="row-item">
                         <Col className="form-group">
                             <label htmlFor="name">Name</label>
-                            <input type="text" name="name" value={state.name} minLength={5} required className="form-control form-control-sm" onChange={handleInputChange}/>
+                            <input type="text" name="name" value={this.state.name} minLength={5} required className="form-control form-control-sm" onChange={this.handleInputChange.bind(this)}/>
                             <FieldFeedbacks for="name">
                                 <FieldFeedback when="valueMissing"> 
                                     You must provide name.
@@ -120,28 +117,28 @@ function Shipping(props) {
                     <Row className="row-item">
                         <Col className="form-group">
                             <label htmlFor="address1">Address 1</label>
-                            <input type="text" required name="address1" value={state.address1} className="form-control form-control-sm" onChange={handleInputChange}/>
+                            <input type="text" required name="address1" value={this.state.address1} className="form-control form-control-sm" onChange={this.handleInputChange.bind(this)}/>
                         </Col>
                         <Col className="form-group">
                             <label htmlFor="address2">Address 2</label>
-                            <input type="text" name="address2" className="form-control form-control-sm" onChange={handleInputChange}/>
+                            <input type="text" name="address2" className="form-control form-control-sm" onChange={this.handleInputChange.bind(this)}/>
                         </Col>
                     </Row>
                     <Row className="row-item">
                         <Col className="form-group">
                             <label htmlFor="phone">Phone Number</label>
-                            <input type="text" required name="phone"  value={state.phone} className="form-control form-control-sm" onChange={handleInputChange}/>
+                            <input type="text" required name="phone"  value={this.state.phone} className="form-control form-control-sm" onChange={this.handleInputChange.bind(this)}/>
                         </Col>
                         <Col className="form-group">
                             <label htmlFor="city">City</label>
-                            <input type="text" required name="city" value={state.city} className="form-control form-control-sm" onChange={handleInputChange}/>
+                            <input type="text" required name="city" value={this.state.city} className="form-control form-control-sm" onChange={this.handleInputChange.bind(this)}/>
                         </Col>
                     </Row>
                     <Row className="row-item">
                         <Col className="form-group shipping-col" xs="6">
                             <label htmlFor="country">Country</label>
                             <br />
-                            <select name="country" required className="form-control form-control-sm"value={state.country}  onChange={handleInputChange} id="country">
+                            <select name="country" required className="form-control form-control-sm"value={this.state.country}  onChange={this.handleInputChange.bind(this)} id="country">
                                 <option value="">Select</option>
                                 {countries.map(country => (
                                     <option key={country.id} value={country.id}>
@@ -153,7 +150,7 @@ function Shipping(props) {
                         <Col className="form-group shipping-col" xs="3">
                             <label htmlFor="province">{provStateLabel}</label>
                             <br />
-                            <select name="province" required className="form-control form-control-sm" value={state.province} onChange={handleInputChange} id="region">
+                            <select name="province" required className="form-control form-control-sm" value={this.state.province} onChange={this.handleInputChange.bind(this)} id="region">
                                 <option value="">Select a {provStateLabel.toLowerCase()}</option>
                                 {regionsForSelectedCountry && regionsForSelectedCountry.length > 0
                                     ? regionsForSelectedCountry.map(region => (
@@ -167,11 +164,11 @@ function Shipping(props) {
                         <Col className="form-group shipping-col" xs="3">
                             <label htmlFor="zip">{postalZipProps.label}</label>
                             <input 
-                            value={state.zip}
+                            value={this.state.zip}
                             name="zip" 
                             type="text" 
                             className="form-control form-control-sm"
-                             onChange={handleInputChange}
+                             onChange={this.handleInputChange.bind(this)}
                              required pattern={postalZipProps.regex}
                               minLength={postalZipProps.minLength}
                                maxLength={postalZipProps.maxLength}
@@ -190,6 +187,7 @@ function Shipping(props) {
                 </FormWithConstraints >
             </div>
         );
+    }
 }
 function mapStateToProps(state) {
     return {
