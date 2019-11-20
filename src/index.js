1465bc4238bc3e510b62b89ac01c1aab6d808c3c
Michael Liendo <mtliendo@gmail.com>
2019-11-19 21:25:36 -0600

    add redux devtools

--- src/index.js
+++ src/index.js
@@ -1,31 +1,33 @@
-import React from 'react';
-import { render } from 'react-dom';
-import './index.css';
-import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
-import 'bootstrap/dist/css/bootstrap.css';
+import React from "react";
+import { render } from "react-dom";
+import "./index.css";
+import App from "./components/App";
+import registerServiceWorker from "./registerServiceWorker";
+import "bootstrap/dist/css/bootstrap.css";
 
-import rootReducer from './reducers';
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import thunkMiddleware from 'redux-thunk';
-import { createLogger } from 'redux-logger';
+import rootReducer from "./reducers";
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+import thunkMiddleware from "redux-thunk";
+import { createLogger } from "redux-logger";
 
 const loggerMiddleware = createLogger();
+const composeEnhancers =
+  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+      })
+    : compose;
 
-const store = createStore(
-    rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-    applyMiddleware(
-        thunkMiddleware,
-        loggerMiddleware
-    )
+const enhancer = composeEnhancers(
+  applyMiddleware(thunkMiddleware, loggerMiddleware)
 );
+const store = createStore(rootReducer, enhancer);
 
 render(
-    <Provider store={ store }>
-        <App />
-    </Provider>,
-    document.getElementById('root')
-)
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
